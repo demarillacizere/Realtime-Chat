942ba86bde7148d6fd6ba6ad1140bbe7964b115e
demarillacizere <izered3@gmail.com>
2024-01-12 19:05:37 +0200

    feat: designing the chat list and chatbox components

--- resources/views/livewire/chat/chat-list.blade.php
+++ resources/views/livewire/chat/chat-list.blade.php
@@ -1,4 +1,32 @@
 <div>
-    {{-- Be like water. --}}
-    This is a chat list
+    <div class="chatlist_header">
+    <div class="title">
+        Chat
+    </div>
+    <div class="img_container">
+        <img src="https://picsum.photos/200/300" alt="">
+    </div>
+    </div>
+    <div class="chatlist_body">
+        <div class="chatlist_item">
+            <div class="chatlist_img_container">
+                <img src="https://picsum.photos/id/100/200/300" alt="">
+            </div>
+            <div class="chatlist_info">
+                <div class="top_row">
+                    <div class="list_username">John</div>
+                    <span class="date"> 2d </span>
+                </div>
+                <div class="bottom_row">
+                    <div class="message_body">
+                        Lorem ipsum dolor sit amet, consecteur
+                    </div>
+                    <div class="unread_count">
+                        56
+                    </div>
+                
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
--- resources/views/livewire/chat/chatbox.blade.php
+++ resources/views/livewire/chat/chatbox.blade.php
@@ -1,3 +1,50 @@
 <div>
-    {{-- Stop trying to control. --}}
+    <div class="chatbox_header">
+        <div class="return">
+            <i class="bi bi-arrow-left"></i> 
+        </div>
+        <div class="img_container">
+            <img src="https://picsum.photos/id/234/200/300" alt="">
+        </div>
+        <div class="name">John</div>
+        <div class="info">
+            <div class="info_item">
+                <i class="bi bi-telephone-fill"></i> 
+            </div>
+            <div class="info_item">
+                <i class="bi bi-image"></i> 
+            </div>
+            <div class="info_item">
+                <i class="bi bi-info-circle-fill"></i> 
+            </div>
+        </div>
+    </div>
+
+    <div class="chatbox_body">
+        <div class="msg_body msg_body_receiver">
+            ghjkasdkfjhgfyuioppoiuytfghjkl;lkjhgfghjkl;lkjhgffghjkllkjhgfghjkjhgfghjkjhgfghjkjhgfghjkjhgfghjk
+            <div class="msg_body_footer">
+                <div class="date">
+                    5 hours ago
+                </div>
+                <div class="read">
+                    <i class="bi bi-check"></i>
+                </div>
+            </div>
+        </div>
+        <div class="msg_body msg_body_me">
+            ghjkasdkfjhgfyuioppoiuytfghjkl;lkjhgfghjkl;lkjhgffghjkllkjhgfghjkjhgfghjkjhgfghjkjhgfghjkjhgfghjk
+            <div class="msg_body_footer">
+                <div class="date">
+                    5 hours ago
+                </div>
+                <div class="read">
+                    <i class="bi bi-check"></i>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="chatbox_footer">
+        lorem ispum
+    </div>
 </div>
