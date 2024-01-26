07c8f2829a935ec73d2d1a1668618b82971831ca
demarillacizere <izered3@gmail.com>
2024-01-26 18:12:12 +0200

    feat: Sending and saving new messages

--- app/Livewire/Chat/SendMessage.php
+++ app/Livewire/Chat/SendMessage.php
@@ -3,16 +3,34 @@
 namespace App\Livewire\Chat;
 
 use App\Models\Conversation;
+use App\Models\Message;
 use App\Models\User;
 use Livewire\Component;
 
 class SendMessage extends Component
 {
+    public $selectedConversation;
+    public $receiverInstance;
+    public $body;
     protected $listeners=['updateSendMessage'];
     function updateSendMessage(Conversation $conversation, User $receiver) {
         $this->selectedConversation = $conversation;
         $this->receiverInstance = $receiver;
         
+    }
+
+    function sendMessage() {
+        if($this->body == null){
+            return null;
+        }
+        $createdMessage = Message::create([
+            'conversation_id'=>$this->selectedConversation->id,
+            'sender_id'=>auth()->id(),
+            'receiver_id'=>$this->receiverInstance->id,
+            'body'=>$this->body,
+        ]);
+        $this->selectedConversation->last_time_message=$createdMessage->created_at;
+        $this->selectedConversation->save();
     }
     public function render()
     {
--- resources/views/livewire/chat/send-message.blade.php
+++ resources/views/livewire/chat/send-message.blade.php
@@ -1,10 +1,12 @@
 <div>
-    <form action="">
+    @if ($selectedConversation)
+    <form wire:submit.prevent='sendMessage' action="">
         <div class="chatbox_footer">
                 <div class="custom_form_group">
-                    <input type="text" class="control" placeholder="Write message...">
+                    <input wire:model='body' type="text" class="control" placeholder="Write message...">
                     <button type="submit" class="submit">Send</button>
                 </div>
         </div>
-</form>
+    </form>
+    @endif
 </div>
