2903bb1b877e7de32094342d21bc79d908ddb52f
demarillacizere <izered3@gmail.com>
2024-02-07 13:06:36 +0200

    Cleaning the welcome page

--- routes/web.php
+++ routes/web.php
@@ -23,9 +23,9 @@ Route::get('/', function () {
 Route::get('users', CreateChat::class)->name('users');
 Route::get('/chat{key?}',Main::class)->name('chat');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
